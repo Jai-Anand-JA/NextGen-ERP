b53214a45e88df30275b28b70ed3568699734db3
01karan10 <karanpatil0206@gmail.com>
2025-07-05 04:04:21 +0530

    StudentDashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import StudentSidebar from './components/StudentSidebar.jsx'
+import StudentDashboard from './Pages/StudentDashboard.jsx'
 function App() {
   return (
     <div>
       {/* <Navbar /> */}
       <StudentSidebar/>
+      <StudentDashboard/>
     </div>
   )
 }
