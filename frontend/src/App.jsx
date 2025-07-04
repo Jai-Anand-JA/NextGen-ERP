f196af1e695f60472ac31b3a5d4a73adb6e9e8da
01karan10 <karanpatil0206@gmail.com>
2025-07-05 01:01:38 +0530

    Navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Navbar from './components/Navbar.jsx'
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
