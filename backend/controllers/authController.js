81afc6a7914b2b5f3106ab3f7150bc792199edf6
Mehra-Jatin <jatinmehra578@gmail.com>
2025-07-05 23:00:02 +0530

    checck-auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,6 +64,44 @@ export const logout = (req, res) => {
     res.status(200).json({ message: "Logout successful" });
 }
 
+export const checkAuth = async (req, res) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        let user;
+
+        if (decoded.role === "Student") {
+            user = await Student.findById(decoded.id).select("-password");
+        } else if (decoded.role === "Faculty") {
+            user = await Faculty.findById(decoded.id).select("-password");
+        } else if (decoded.role === "Admin") {
+            user = await Admin.findById(decoded.id).select("-password");
+        }
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "User authenticated",
+            user: {
+                id: user._id,
+                role: user.role,
+            },
+        });
+    }
+    catch (error) {
+        console.error("Check auth error:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
+
 export const getCurrentUser = async (req, res) => {
     try {
         const token = req.cookies.token;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { authMiddleware } from '../middleware/authMiddleware.js';
-import { getCurrentUser, login, logout } from '../controllers/authController.js';
+import { checkAuth, getCurrentUser, login, logout } from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/login',login);
 router.post('/logout',logout);
-
+router.get('/check-auth', checkAuth);
 router.get('/profile',getCurrentUser);
 
 export default router;
